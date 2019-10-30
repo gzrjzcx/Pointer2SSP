7630dafd851e3141c96d4fe675197b9be99943e7
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-10-30 05:37:06 +0000

    Deploy gzrjzcx/Pointer2SSP to github.com/gzrjzcx/Pointer2SSP.git:gh-pages

---
+++ gitalk_config.js
@@ -0,0 +1,9 @@
+    var gitalk = new Gitalk({
+        clientID: '891633df9540ec724578',
+        clientSecret: '012103e75c027bbbe2b6687fb84be518d7bb9994',
+        repo: 'Pointer2SSP',
+        owner: 'gzrjzcx',
+        admin: ['gzrjzcx'],
+        id: md5(location.pathname),
+    });
+    gitalk.render('gitalk-container');
